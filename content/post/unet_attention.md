e2489c4c91a0fbad39e4c9783342ccef3dd28df8
Taiki <horiuchi_tik@yahoo.co.jp>
2019-11-19 16:31:35 +0900

    aaa

---
+++ content/post/unet_attention.md
@@ -0,0 +1,6 @@
+---
+title: "Unet_attention"
+date: 2019-08-27
+tags: []
+draft: false
+---
--- docs/post/unet_attention/build/static/bizstyle.js
+++ docs/post/unet_attention/build/static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("project_name 1.0.0 ドキュメント");
+        $("li.nav-item-0 a").text("unet_attention 1.0.0 ドキュメント");
     }
 });
